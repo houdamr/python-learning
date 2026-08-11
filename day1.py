7cafab23d2cfd01d3f7aef3a6c149d11b9ee11e8
houdamr <mrh922360@gmail.com>
2026-08-11 23:48:52 +0100

    Add files via upload
    add day 1 python basics

---
+++ day1.py
@@ -0,0 +1,41 @@
+name = "houda"
+age = 18
+height = 1.59
+print("hey! my name is ", name ,",i am ", age,"years old , my height is ", height ,", yes!, iam student")
+
+a = 15
+b = 7
+print("a + b =", a+b)
+print("a - b =", a-b)
+print("a * b =",a*b)
+print("a / b =",a/b)
+
+name = "houda"
+age = 18
+print("my name is",name)
+print("i am",age,"years old")
+print("i study Computer science")
+print(" i want to become an AI Engineer")
+
+price = 250
+quantity = 3
+print("the total price is",price*quantity)
+discount = 50
+print("the final price is",price*quantity-discount)
+
+name = input("what is your name? ")
+age = int(input("how old are you "))
+print(name,", you are",age,"years old")
+
+print("after 5 years , you will be",age+5,"years old.")
+
+if age>= 18:
+    print("you are an adult")
+else:
+    print("you are under 18")
+
+grade = float(input("enter your grade: "))
+if grade>=10:
+    print("you are passed!")
+else:
+    print("you failed")
