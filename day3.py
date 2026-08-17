3c25c9c5b8a5ec77b32ac8cf0ee906c201e2bf64
houdamr <mrh922360@gmail.com>
2026-08-17 10:34:06 +0100

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,34 @@
+i=1
+while i<=10:
+    print(i)
+    i=i+1
+
+for i in range(1,11):
+    print(i)
+
+i=2
+while i<=20:
+    print(i)
+    i=i+2
+
+n=int(input("enter a number: "))
+for i in range(1,n+1):
+    print(i)
+
+n2=int(input("enter a number : "))
+sum=0
+for i in range(1,n2+1):
+    sum=sum+i
+print(sum)
+
+n3=int(input("enter a number:"))
+i=n3
+while i>=0:
+    print(i)
+    i=i-1
+
+n4=int(input("enter a number : "))
+for i in range(1,11):
+    print(n4,"*",i,"=",n4*i)
+
+
